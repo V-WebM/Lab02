36e502495a073f8220083d822c198185f513e3b5
Ventura <marlenp363@gmail.com>
2022-03-30 16:45:36 -0500

    C02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -14,23 +14,6 @@ namespace Lab02
             {
                 Rectangulo rectangulo = new Rectangulo();
 
-                //Console.WriteLine("Ingresar x1");
-                //double x1 = rectangulo.x1 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar y1");
-                //double y1 = rectangulo.y1 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar x2");
-                //double x2 = rectangulo.x2 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar y2");
-                //double y2 = rectangulo.y2 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar x3");
-                //double x3 = rectangulo.x3 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar y3");
-                //double y3 = rectangulo.y3 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar x4");
-                //double x4 = rectangulo.x4 = Convert.ToDouble(Console.ReadLine());
-                //Console.WriteLine("Ingresar y4");
-                //double y4 = rectangulo.y4 = Convert.ToDouble(Console.ReadLine());
-
                 Console.WriteLine("Ingresar la coordenada del primer punto");
                 double x1 = Double.Parse(Console.ReadLine());
                 //Console.WriteLine("Ingresar y1");
