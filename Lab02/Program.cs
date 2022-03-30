ffcbebf555b552cf2c94674a057e80f6c3ebffc8
Ventura <marlenp363@gmail.com>
2022-03-30 16:04:36 -0500

    Add project files.

---
+++ Lab02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rectangulo rectangulo = new Rectangulo();
+
+                Console.WriteLine("Ingresar x1");
+                double x1 = rectangulo.x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar y1");
+                double y1 = rectangulo.y1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar x2");
+                double x2 = rectangulo.x2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar y2");
+                double y2 = rectangulo.y2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar x3");
+                double x3 = rectangulo.x3 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar y3");
+                double y3 = rectangulo.y3 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar x4");
+                double x4 = rectangulo.x4 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingresar y4");
+                double y4 = rectangulo.y4 = Convert.ToDouble(Console.ReadLine());
+
+                double bs;
+                bs = rectangulo.Base(x2, x1, y1, y2);
+
+                double h;
+                h = rectangulo.Altura(x3, y3, y4, x4);
+
+                double area;
+                area = rectangulo.Area(bs, h);
+
+                double perimetro;
+                perimetro = rectangulo.Perimetro(bs, h);
+
+                Console.WriteLine("El area del rectangulo es :" + area);
+                Console.WriteLine("El perimetro es:" + perimetro);
+
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+        }
+    }
+}
+
---
+++ Lab02/Rectangulo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Rectangulo
+    {
+        public double x1 { get; set; }
+        public double y1 { get; set; }
+        public double x2 { get; set; }
+        public double y2 { get; set; }
+        public double x3 { get; set; }
+        public double y3 { get; set; }
+        public double x4 { get; set; }
+        public double y4 { get; set; }
+
+
+        public double Base(double x2, double x1, double y1, double y2)
+        {
+            double bs = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return bs;
+        }
+
+        public double Altura(double x3, double y3, double y4, double x4)
+        {
+            double h = Math.Sqrt(Math.Pow((x3 - x4), 2) + Math.Pow((y3 - y4), 2));
+            return h;
+        }
+
+        public double Perimetro(double bs, double h)
+        {
+            double p = (2 * bs) + (2 * h);
+            return p;
+        }
+
+        public double Area(double bs, double h)
+        {
+            double a = bs * h;
+            return a;
+        }
+    }
+}
+
+
